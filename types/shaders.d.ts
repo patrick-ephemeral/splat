5b7e8b81481436ad912e57007ca1208c381d4ca5
Patrick <patrick@ephemeral-software.com>
2025-09-06 17:20:40 -0400

    Typescript declaration for glsl files

--- types/shaders.d.ts
+++ types/shaders.d.ts
@@ -10,3 +10,8 @@ declare module "*.frag" {
     const src: string;
     export default src;
 }
+
+// With ?raw
+declare module "*.glsl?raw" { const src: string; export default src; }
+declare module "*.vert?raw" { const src: string; export default src; }
+declare module "*.frag?raw" { const src: string; export default src; }
